d5f36f21a2b3e7afc87eda11c1c19ec3e061016a
Krišjānis Veinbahs <krisjanis.veinbahs@gmail.com>
2017-06-21 08:57:08 +0300

    [0.1.0] Adding renderer, controls, finished backend.

---
+++ changelog.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+### [0.1.0]
+- Create tetris engine
+  - Create timed main computation loop
+  - Create randomized, seed-based tetris dispenser
+  - Create tetromino transformation module
+  - Create basic tetris game logic
+  - Create engine interface
+- Create control system
+- Create rendering system 
---
+++ www/app/engine/lib/hook/hook.js
@@ -0,0 +1,30 @@
+define([
+    "require",
+],
+function(
+    require
+) {
+    return function Hook() {
+        this.before = function(callback) {
+            this._befores.push(callback)
+            return this._befores.length - 1 // Action id
+        }
+        this._runBefores = function(engine, delay) {
+            for (i = 0; i < this._befores.length; i++) {
+                this._befores[i](engine, delay)
+            }
+        }
+        this._befores = new Array() // Functions to run before each frame
+        this.after = function(callback) {
+            this._afters.push(callback)
+            return this._afters.length - 1 // Action id
+        }
+        this._runAfters = function(engine, delay) {
+            for (i = 0; i < this._afters.length; i++) {
+                this._afters[i](engine, delay)
+            }
+        }
+        this._afters = new Array() // Functions to run after each frame
+    }
+})
+
